fdf0aa681c539d3e4f981f1626016c32c371c711
shzalen <shzalen@163.com>
2026-07-06 10:00:47 +0800

    构建新版本

---
+++ root/assets/swimlanesDiagram-G3AALYLV-D1V0E2sw.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-C9bZ9X6A.js";import{_ as o}from"./mermaid.core-txkqLI22.js";import"./transform-BjnKP4GC.js";import"./chunk-5VM5RSS4-BnfqzlUZ.js";import"./chunk-XXDRQBXY-C2TGu6gh.js";import"./chunk-VR4S4FIN-Dv5VPz0c.js";import"./chunk-32BRIVSS-DXk5djFr.js";import"./purify.es-BeiWAugl.js";import"./channel-D7xuigNv.js";import"./vendor-tiptap-custom-ZGLNjnSP.js";import"./vendor-tiptap-core-CAbOIzkA.js";import"./vendor-prosemirror-Bv-9WTiV.js";import"./_commonjsHelpers-CqkleIqs.js";var a=o(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,w=r({defaultLayout:"swimlane",styles:m});export{w as diagram};
