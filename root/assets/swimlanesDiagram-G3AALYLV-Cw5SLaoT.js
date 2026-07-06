cd252bee96ebe6830b6cd23fbc39535928c3f272
shzalen <shzalen@163.com>
2026-07-06 09:24:39 +0800

    add root doc

---
+++ root/assets/swimlanesDiagram-G3AALYLV-Cw5SLaoT.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-qCuUTTEP.js";import{_ as o}from"./mermaid.core-DYo39Z_A.js";import"./transform-BjnKP4GC.js";import"./chunk-5VM5RSS4-BjtDTRA2.js";import"./chunk-XXDRQBXY-ChdnXTD8.js";import"./chunk-VR4S4FIN-CvTMENfF.js";import"./chunk-32BRIVSS-D8c3LQqK.js";import"./purify.es-BeiWAugl.js";import"./channel-rfcAMADb.js";import"./vendor-tiptap-custom-BOLJL5uD.js";import"./vendor-tiptap-core-CAbOIzkA.js";import"./vendor-prosemirror-Bv-9WTiV.js";import"./_commonjsHelpers-CqkleIqs.js";var a=o(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,w=r({defaultLayout:"swimlane",styles:m});export{w as diagram};
