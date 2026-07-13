4c93b0611ce750f146ab4f440be02a88ecdc3182
shzalen <shzalen@163.com>
2026-07-13 14:10:29 +0800

    update

---
+++ assets/swimlanesDiagram-G3AALYLV-C-P7mMro.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-6WlFTgmg.js";import{_ as o}from"./mermaid.core-CnsBFf-z.js";import"./transform-BjnKP4GC.js";import"./chunk-5VM5RSS4-Bp0RPrPM.js";import"./chunk-XXDRQBXY-B5CatXaU.js";import"./chunk-VR4S4FIN-1kqODegW.js";import"./chunk-32BRIVSS-0edKkrfo.js";import"./vendor-markdown-DMtiLmeG.js";import"./channel-CKeQRvjo.js";import"./index-DRWnnbhM.js";import"./vendor-vue-nusXZLve.js";import"./vendor-supabase-CaRhJy_T.js";import"./vendor-lunar-uYts24a8.js";var a=o(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,w=r({defaultLayout:"swimlane",styles:m});export{w as diagram};
