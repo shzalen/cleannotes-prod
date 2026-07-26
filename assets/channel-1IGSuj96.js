d52044710c1ada5ab78b6da624d93b8309c4652d
alen.liu <alen.liu@zuru.com>
2026-07-26 12:24:55 +0800

    fix: Android H5 — 顶部色块/任务点击/用户手册

--- assets/channel-1IGSuj96.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"./mermaid.core-CWlc7QcX.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/swimlanesDiagram-G3AALYLV-DwE3u7JF.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-CvzV_5Bd.js";import{_ as o}from"./mermaid.core-CN4zGbkX.js";import"./transform-BjnKP4GC.js";import"./chunk-5VM5RSS4-cZjBZPO-.js";import"./chunk-XXDRQBXY-DDRFaVSZ.js";import"./chunk-VR4S4FIN-6mdFLbCr.js";import"./chunk-32BRIVSS-D5XEWdRd.js";import"./vendor-markdown-CGju0iDY.js";import"./channel-BYZC6Api.js";import"./_plugin-vue_export-helper-Bw4bWBtg.js";import"./vendor-vue-GDyxEOhH.js";import"./vendor-supabase-DECvRwsC.js";import"./vendor-lunar-uYts24a8.js";var a=o(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,w=r({defaultLayout:"swimlane",styles:m});export{w as diagram};
